133a06b5bfe79ab650cc917e90d33df7b4070cc2
KnoxDevelopment <142834101+KnoxDevelopment@users.noreply.github.com>
2023-08-21 20:29:15 +0200

    Create functions.lua

---
+++ Server/functions.lua
@@ -0,0 +1,6 @@
+ESX = exports['es_extended']:getSharedObject()
+KnoxDev_CarThief_SV = {}
+KnoxDev_CarThief_SV.CarThief = false
+KnoxDev_CarThief_SV.Waiting = false
+KnoxDev_CarThief_SV.Coords = nil
+KnoxDev_CarThief_SV.Player = nil
